ae9e538310af6489430ffa2a6245edbc5f977d33
g-shaburov-nag <g.shaburov@nag.ru>
2022-10-19 22:01:17 +0500

    Заменил lsp-installer на mason, т.к. первый перестал поддерживаться

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -87,8 +87,8 @@ cmp.setup({
 -- })
 
 -- Setup lspconfig.
-M.capabilities = require('cmp_nvim_lsp').update_capabilities(
-	vim.lsp.protocol.make_client_capabilities()
+M.capabilities = require('cmp_nvim_lsp').default_capabilities(
+	-- vim.lsp.protocol.make_client_capabilities()
 )
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require('lspconfig')[''].setup {
