06e0da0207a7b53a2b6e6331d66dff4744aea406
jiorm <hiehiohi@gmail.com>
2023-08-04 19:21:16 +0500

    statusline

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -8,36 +8,36 @@ end
 
 function sl_get_lsp_message()
 	local result = ""
-	local message = lsp_status.messages()
-	local bnum = vim.api.nvim_get_current_buf()
-	local bname = vim.api.nvim_buf_get_name(bnum)
-	-- Не совсем правильно т.к. у файлов могут быть одинаковые названия.
-	-- local filename1 = sl_get_file_name(bname)
+	-- local message = lsp_status.messages()
+	-- local bnum = vim.api.nvim_get_current_buf()
+	-- local bname = vim.api.nvim_buf_get_name(bnum)
+	-- -- Не совсем правильно т.к. у файлов могут быть одинаковые названия.
+	-- -- local filename1 = sl_get_file_name(bname)
 	
-	for i, msg in ipairs(message) do
-		if i ~= 1 then
-			result = result .. ';'
-		end
+	-- for i, msg in ipairs(message) do
+	-- 	if i ~= 1 then
+	-- 		result = result .. ';'
+	-- 	end
 
-		if msg.name == 'clangd' and msg.uri then
-			-- local filename2 = sl_get_file_name(msg.uri)
-			-- if filename1 == filename2 then
-				result = result .. ' clangd'
-				if msg.content ~= 'idle' then
-					result = result .. '...'
-				end
-				-- Решил пока не показывать текст, т.к. мерцает при
-				-- редактировании файла.
-				-- if msg.content then result = result .. ' ' .. msg.content  end
-			-- end
-		else
-			if msg.title then result =  result .. ' ' .. msg.title end
-			if msg.message then result = result .. ' ' .. msg.message end
-			if msg.percentage then
-				result = result .. string.format(' (%.0f%%)', msg.percentage)
-			end
-		end
-	end
+	-- 	if msg.name == 'clangd' and msg.uri then
+	-- 		-- local filename2 = sl_get_file_name(msg.uri)
+	-- 		-- if filename1 == filename2 then
+	-- 			result = result .. ' clangd'
+	-- 			if msg.content ~= 'idle' then
+	-- 				result = result .. '...'
+	-- 			end
+	-- 			-- Решил пока не показывать текст, т.к. мерцает при
+	-- 			-- редактировании файла.
+	-- 			-- if msg.content then result = result .. ' ' .. msg.content  end
+	-- 		-- end
+	-- 	else
+	-- 		if msg.title then result =  result .. ' ' .. msg.title end
+	-- 		if msg.message then result = result .. ' ' .. msg.message end
+	-- 		if msg.percentage then
+	-- 			result = result .. string.format(' (%.0f%%)', msg.percentage)
+	-- 		end
+	-- 	end
+	-- end
 
 	return result
 end
