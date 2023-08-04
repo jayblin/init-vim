d8bbbf83d2104e0ca7d4a2f65874b9e76f4828fd
jiorm <hiehiohi@gmail.com>
2023-08-04 19:21:51 +0500

    lsp svelte

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -79,6 +79,11 @@ require('lspconfig')['clangd'].setup{
 	handlers = lsp_status.extensions.clangd.setup(),
 }
 
+require('lspconfig')['svelte'].setup{
+    on_attach = on_attach,
+    capabilities = u_cmp.capabilities,
+}
+
 -- local lsp_installer = require("nvim-lsp-installer")
 
 -- lsp_installer.on_server_ready(function(server)
